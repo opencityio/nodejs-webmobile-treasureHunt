0d43f5a62cc04764ee5f3213e69f143acbe3fcda
Josh Brown <josh@joossh.com>
2015-02-13 14:50:27 +0000

    Added port number by nconf

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ require('newrelic');
 var express = require('express')
 var app = express()
 
+nconf
+  .argv()
+  .env()
+  .file('local', 'config/local.json')
+  .file('user', 'config/' + environment + '.json')
+  .file('global', 'config/global.json')
+  .file('package', 'package.json')
+  .set('ENVIRONMENT', environment);
+
 app.use('/components/', express.static(__dirname + '/bower_components'));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/app.html');
-})
+});
 
-var server = app.listen(3000, function () {
+var server = app.listen(nconf.get('PORT'), function () {
 
   var host = server.address().address
   var port = server.address().port
